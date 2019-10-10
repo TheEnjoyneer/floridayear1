8a16e20b6dd2d160e785a6bc3eaf59b2243099f3
Christopher Brant <cbrant@g.clemson.edu>
2019-10-10 15:25:15 -0400

    think I have the client stub written...

--- fall2019/eel5737/HW4/client_stub.py
+++ fall2019/eel5737/HW4/client_stub.py
@@ -21,104 +21,44 @@ class client_stub():
 
 	#REQUEST TO FETCH THE INODE FROM INODE NUMBER FROM SERVER
 	def inode_number_to_inode(inode_number):
-		# Marshal the data to be sent
-		m_inode_number = # marshalled inode_number
-
-		# Call the server inode_number_to_inode after marshalling the data correctly
-		m_inode = proxy.inode_number_to_inode(m_inode_number)
-
-		# Marshal the retData after returning from the server call
-		inode = # unmarshalled inode object
-
 		# Return the correct data
-	    return inode
+	    return pickle.loads(proxy.inode_number_to_inode(pickle.dumps(inode_number)))
 
 
 	#REQUEST THE DATA FROM THE SERVER
 	def get_data_block(block_number):
-		# Marshal the data to be sent
-		m_block_number = # marshalled block_number
-
-		# Call the server get_data_block after marshalling the data correctly
-		m_block_data = proxy.get_data_block(block_number)
-
-		# Marshal the retData after returning from the server call
-		block_data = # unmarshalled block data
-
 		# Return the correct data
-	    return block_data
+	    return pickle.loads(proxy.get_data_block(pickle.dumps(block_number)))
 
 
 	#REQUESTS THE VALID BLOCK NUMBER FROM THE SERVER 
 	def get_valid_data_block():
-	    # Call the server get_valid_data_block
-	    m_valid_block = proxy.get_valid_data_block()
-
-		# Marshal the retData after returning from the server call
-		valid_block = # unmarshalled valid block number
-
 		# Return the correct data
-	    return valid_block
+	    return pickle.loads(proxy.get_valid_data_block())
 
 
 	#REQUEST TO MAKE BLOCKS RESUABLE AGAIN FROM SERVER
 	def free_data_block(block_number):
-		# Marshal the data to be sent
-		m_block_number = # marshalled block_number
-
-	    # Call the server free_data_block after marshalling the data correctly
-	    m_retErr = proxy.free_data_block(m_block_number)
-
-	    # Marshal the retErr after returning from the server call
-	    retErr = # unmarshall m_retErr
-
 	    # Return the possible error, if no error, set retErr to 0
-	    return retErr
+	    return pickle.loads(proxy.free_data_block(pickle.dumps(block_number)))
 
 
 	#REQUEST TO WRITE DATA ON THE THE SERVER
 	def update_data_block(block_number, block_data):
-		# Marshal the data to be sent
-		m_block_number = # marshalled block_number
-		m_block_data = # marshalled block_data
-
-	    # Call the server update_data_block after marshalling the data correctly
-	    m_retErr = proxy.update_data_block(m_block_number, m_block_data)
-
-		# Marshal the retErr after returning from the server call
-	    retErr = # unmarshall m_retErr
-
 		# Return the possible error, if no error, set retErr to 0
-	    return retErr
+	    return pickle.loads(proxy.update_data_block(pickle.dumps(block_number), pickle.dumps(block_data)))
 
 
 	#REQUEST TO UPDATE THE UPDATED INODE IN THE INODE TABLE FROM SERVER
 	def update_inode_table(inode, inode_number):
-		# Marshal the data to be sent
-		m_inode = # marshalled inode
-		m_inode_number = # marshalled inode_number
-
-	    # Call the server update_inode_table after marshalling the data correctly
-	    m_retErr = proxy.update_inode_table(m_inode, m_inode_number)
-
-
-		# Marshal the retErr after returning from the server call
-	    retErr = # unmarshall m_retErr
-
 		# Return the possible error, if no error, set retErr to 0
-	    return retErr
+	    return pickle.loads(proxy.update_inode_table(pickle.dumps(inode), pickle.dumps(inode_number)))
 
 
 	#REQUEST FOR THE STATUS OF FILE SYSTEM FROM SERVER
 	def status():
-	    # Call the server status
-	    m_statusString = proxy.status()
-
-		# Marshal the retData after returning from the server call
-		statusString = # unmarshalled m_statusString
-
-		# Print out the status to the terminal
-		print statusString
+		# Return the status string after marshalling the data
+		return pickle.loads(proxy.status())
 
 
 
--- fall2019/eel5737/HW4/server_stub.py
+++ fall2019/eel5737/HW4/server_stub.py
@@ -29,102 +29,110 @@ def Initialize():
 
 	#GIVES ADDRESS OF INODE TABLE
 	def addr_inode_table(self):				
-		return sblock.ADDR_INODE_BLOCKS
+		# return sblock.ADDR_INODE_BLOCKS
 
 
 	#RETURNS THE DATA OF THE BLOCK
 	def get_data_block(self, block_number):	
-		if block_number == 0: print("Memory: Reserved for Boot Block")
-		elif block_number == 1: print("Memory: Reserved for Super Block")
-		elif block_number >= sblock.BITMAP_BLOCKS_OFFSET and block_number < sblock.INODE_BLOCKS_OFFSET:
-			return sblock.ADDR_BITMAP_BLOCKS[block_number - sblock.BITMAP_BLOCKS_OFFSET].block
-		elif block_number >= sblock.INODE_BLOCKS_OFFSET and block_number < sblock.DATA_BLOCKS_OFFSET:
-			return sblock.ADDR_INODE_BLOCKS[block_number - sblock.INODE_BLOCKS_OFFSET].block
-		elif block_number >= sblock.DATA_BLOCKS_OFFSET and block_number < sblock.TOTAL_NO_OF_BLOCKS:
-			return sblock.ADDR_DATA_BLOCKS[block_number - sblock.DATA_BLOCKS_OFFSET].block
-		else: print("Memory: Block index out of range or Wrong input!")
-		return -1
+		# Unmarshal the data for block_number
+		block_number = pickle.loads(block_number)
+
+		# Find the data at block_number and marshal it upon a return call
+		print block_number
+
+		return pickle.dumps(block_number)
+
+		# if block_number == 0: print("Memory: Reserved for Boot Block")
+		# elif block_number == 1: print("Memory: Reserved for Super Block")
+		# elif block_number >= sblock.BITMAP_BLOCKS_OFFSET and block_number < sblock.INODE_BLOCKS_OFFSET:
+		# 	return pickle.dumps(sblock.ADDR_BITMAP_BLOCKS[block_number - sblock.BITMAP_BLOCKS_OFFSET].block)
+		# elif block_number >= sblock.INODE_BLOCKS_OFFSET and block_number < sblock.DATA_BLOCKS_OFFSET:
+		# 	return pickle.dumps(sblock.ADDR_INODE_BLOCKS[block_number - sblock.INODE_BLOCKS_OFFSET].block)
+		# elif block_number >= sblock.DATA_BLOCKS_OFFSET and block_number < sblock.TOTAL_NO_OF_BLOCKS:
+		# 	return pickle.dumps(sblock.ADDR_DATA_BLOCKS[block_number - sblock.DATA_BLOCKS_OFFSET].block)
+		# else: print("Memory: Block index out of range or Wrong input!")
+		# return pickle.dumps(-1)
 
 
 	#RETURNS THE BLOCK NUMBER OF AVAIALBLE DATA BLOCK  
 	def get_valid_data_block(self):			
-		for i in range(0, sblock.TOTAL_NO_OF_BLOCKS):
-			if sblock.ADDR_BITMAP_BLOCKS[i / sblock.BLOCK_SIZE].block[i % sblock.BLOCK_SIZE] == 0:
-				sblock.ADDR_BITMAP_BLOCKS[i / sblock.BLOCK_SIZE].block[i % sblock.BLOCK_SIZE] = 1
-				return i
-		print("Memory: No valid blocks available")
-		return -1
+		# for i in range(0, sblock.TOTAL_NO_OF_BLOCKS):
+		# 	if sblock.ADDR_BITMAP_BLOCKS[i / sblock.BLOCK_SIZE].block[i % sblock.BLOCK_SIZE] == 0:
+		# 		sblock.ADDR_BITMAP_BLOCKS[i / sblock.BLOCK_SIZE].block[i % sblock.BLOCK_SIZE] = 1
+		# 		return i
+		# print("Memory: No valid blocks available")
+		# return -1
 
 	#REMOVES THE INVALID DATA BLOCK TO MAKE IT REUSABLE
 	def free_data_block(self, block_number):  	
-		if block_number > 0:
-			sblock.ADDR_BITMAP_BLOCKS[block_number / sblock.BLOCK_SIZE].block[block_number % sblock.BLOCK_SIZE] = 0
-			b = sblock.ADDR_DATA_BLOCKS[block_number - sblock.DATA_BLOCKS_OFFSET].block
-			for i in range(0, sblock.BLOCK_SIZE): b[i] = '\0'
+		# if block_number > 0:
+		# 	sblock.ADDR_BITMAP_BLOCKS[block_number / sblock.BLOCK_SIZE].block[block_number % sblock.BLOCK_SIZE] = 0
+		# 	b = sblock.ADDR_DATA_BLOCKS[block_number - sblock.DATA_BLOCKS_OFFSET].block
+		# 	for i in range(0, sblock.BLOCK_SIZE): b[i] = '\0'
 
 
 	#WRITES TO THE DATA BLOCK
 	def update_data_block(self, block_number, block_data):		
-		b = sblock.ADDR_DATA_BLOCKS[block_number - sblock.DATA_BLOCKS_OFFSET].block
-		for i in range(0, len(block_data)): b[i] = block_data[i]
-		#print("Memory: Data Copy Completes")
+		# b = sblock.ADDR_DATA_BLOCKS[block_number - sblock.DATA_BLOCKS_OFFSET].block
+		# for i in range(0, len(block_data)): b[i] = block_data[i]
+		# #print("Memory: Data Copy Completes")
 	
 	
 	#UPDATES INODE TABLE WITH UPDATED INODE
 	def update_inode_table(self, inode, inode_number):
-		sblock.ADDR_INODE_BLOCKS[inode_number / sblock.INODES_PER_BLOCK].block[inode_number % sblock.INODES_PER_BLOCK] = inode
+		# sblock.ADDR_INODE_BLOCKS[inode_number / sblock.INODES_PER_BLOCK].block[inode_number % sblock.INODES_PER_BLOCK] = inode
 
 	
 	#RETURNS THE INODE FROM INODE NUMBER
 	def inode_number_to_inode(self, inode_number):
-		return sblock.ADDR_INODE_BLOCKS[inode_number / sblock.INODES_PER_BLOCK].block[inode_number % sblock.INODES_PER_BLOCK]
+		# return sblock.ADDR_INODE_BLOCKS[inode_number / sblock.INODES_PER_BLOCK].block[inode_number % sblock.INODES_PER_BLOCK]
 
 	
 	#SHOWS THE STATUS OF DISK LAYOUT IN MEMORY
 	def status(self):
-		counter = 1
-		string = ""
-		string += "\n----------BITMAP: ----------(Block Number : Valid Status)\n"
-		block_number = 0
-		for i in range(2, sblock.INODE_BLOCKS_OFFSET):
-			string += "Bitmap Block : " + str(i - 2) + "\n"
-			b = sblock.ADDR_BITMAP_BLOCKS[i - sblock.BITMAP_BLOCKS_OFFSET].block
-			for j in range(0, len(b)):
-				if j == 20: break   #only to avoid useless data to print
-				string += "\t\t[" + str(block_number + j) + "  :  "  + str(b[j]) + "]  \n"
-			block_number += len(b)
-			if counter == 1: break
-		string += ".....showing just part(20) of 1st bitmap block!\n"
+		# counter = 1
+		# string = ""
+		# string += "\n----------BITMAP: ----------(Block Number : Valid Status)\n"
+		# block_number = 0
+		# for i in range(2, sblock.INODE_BLOCKS_OFFSET):
+		# 	string += "Bitmap Block : " + str(i - 2) + "\n"
+		# 	b = sblock.ADDR_BITMAP_BLOCKS[i - sblock.BITMAP_BLOCKS_OFFSET].block
+		# 	for j in range(0, len(b)):
+		# 		if j == 20: break   #only to avoid useless data to print
+		# 		string += "\t\t[" + str(block_number + j) + "  :  "  + str(b[j]) + "]  \n"
+		# 	block_number += len(b)
+		# 	if counter == 1: break
+		# string += ".....showing just part(20) of 1st bitmap block!\n"
 
-		string += "\n\n----------INODE Blocks: ----------(Inode Number : Inode(Address)\n"
-		inode_number = 0
-		for i in range(sblock.INODE_BLOCKS_OFFSET, sblock.DATA_BLOCKS_OFFSET):
-			string += "Inode Block : " + str(i - sblock.INODE_BLOCKS_OFFSET) + "\n"
-			b = sblock.ADDR_INODE_BLOCKS[i - sblock.INODE_BLOCKS_OFFSET].block
-			for j in range(0, len(b)):
-				string += "\t\t[" + str(inode_number + j) + "  :  "  + str(bool(b[j])) + "]  \n"
-			inode_number += len(b)
+		# string += "\n\n----------INODE Blocks: ----------(Inode Number : Inode(Address)\n"
+		# inode_number = 0
+		# for i in range(sblock.INODE_BLOCKS_OFFSET, sblock.DATA_BLOCKS_OFFSET):
+		# 	string += "Inode Block : " + str(i - sblock.INODE_BLOCKS_OFFSET) + "\n"
+		# 	b = sblock.ADDR_INODE_BLOCKS[i - sblock.INODE_BLOCKS_OFFSET].block
+		# 	for j in range(0, len(b)):
+		# 		string += "\t\t[" + str(inode_number + j) + "  :  "  + str(bool(b[j])) + "]  \n"
+		# 	inode_number += len(b)
 		
-		string += "\n\n----------DATA Blocks: ----------\n  "
-		counter = 0
-		for i in range(sblock.DATA_BLOCKS_OFFSET, sblock.TOTAL_NO_OF_BLOCKS):
-			if counter == 25: 
-				string += "......Showing just part(25) data blocks\n"
-				break
-			string += (str(i) + " : " + "".join(sblock.ADDR_DATA_BLOCKS[i - sblock.DATA_BLOCKS_OFFSET].block)) + "  "
-			counter += 1
+		# string += "\n\n----------DATA Blocks: ----------\n  "
+		# counter = 0
+		# for i in range(sblock.DATA_BLOCKS_OFFSET, sblock.TOTAL_NO_OF_BLOCKS):
+		# 	if counter == 25: 
+		# 		string += "......Showing just part(25) data blocks\n"
+		# 		break
+		# 	string += (str(i) + " : " + "".join(sblock.ADDR_DATA_BLOCKS[i - sblock.DATA_BLOCKS_OFFSET].block)) + "  "
+		# 	counter += 1
 
 		
-		string += "\n\n----------HIERARCHY: ------------\n"
-		for i in range(sblock.INODE_BLOCKS_OFFSET, sblock.DATA_BLOCKS_OFFSET):
-			for j in range(0, sblock.INODES_PER_BLOCK):
-				inode = sblock.ADDR_INODE_BLOCKS[i-sblock.INODE_BLOCKS_OFFSET].block[j]
-				if inode and inode[0]:
-					string += "\nDIRECTORY: " + inode[1] + "\n"
-					for x in inode[7]: string += "".join(x[:config.MAX_FILE_NAME_SIZE]) + " || "
-					string += "\n"
+		# string += "\n\n----------HIERARCHY: ------------\n"
+		# for i in range(sblock.INODE_BLOCKS_OFFSET, sblock.DATA_BLOCKS_OFFSET):
+		# 	for j in range(0, sblock.INODES_PER_BLOCK):
+		# 		inode = sblock.ADDR_INODE_BLOCKS[i-sblock.INODE_BLOCKS_OFFSET].block[j]
+		# 		if inode and inode[0]:
+		# 			string += "\nDIRECTORY: " + inode[1] + "\n"
+		# 			for x in inode[7]: string += "".join(x[:config.MAX_FILE_NAME_SIZE]) + " || "
+		# 			string += "\n"
 		
-		return string
+		# return string
 
 
 # Begin server listening
