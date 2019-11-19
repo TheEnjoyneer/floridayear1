812f85d96900d764ed03ef5007f060594fbb229f
Christopher Brant <cbrant@g.clemson.edu>
2019-11-19 18:56:59 -0500

    debugging

--- fall2019/eel5737/finalProject/raidcontroller.py
+++ fall2019/eel5737/finalProject/raidcontroller.py
@@ -216,6 +216,7 @@ class raidController():
 
     def checksum_to_data(self, block_data):
         # Runs checksum check, if it is bad, returns a -1
+        print block_data
         if block_data == None:
             return ""
         old_block_data = block_data[:-16]
