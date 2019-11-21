740329692cf11475353250fe41c6adaca7bff32b
Christopher Brant <cbrant@g.clemson.edu>
2019-11-21 14:39:51 -0500

    debugging

--- fall2019/eel5737/finalProject/raidcontroller.py
+++ fall2019/eel5737/finalProject/raidcontroller.py
@@ -17,7 +17,7 @@ def xor_strings(string1, string2):
         xor_response[i] = s1[i] ^ s2[i]
     # Decode the response
     retString = xor_response.decode()
-    return retString
+    return str(retString)
 
     #return "".join(chr(ord(a) ^ ord(b)) for a,b in zip(string1, string2))
 
