1da2a61da245ae42ca9e3610e537f89762195438
Christopher Brant <cbrant@g.clemson.edu>
2019-11-25 10:21:04 -0500

    debugging

--- fall2019/eel5737/finalProject/raidcontroller.py
+++ fall2019/eel5737/finalProject/raidcontroller.py
@@ -4,11 +4,11 @@ portNum = 8000
 maxDataBlocks = ((config.INODE_SIZE - 63 - config.MAX_FILE_NAME_SIZE) / 2) * (config.NUM_OF_SERVERS - 1)
 
 def xor_strings(string1, string2):
-    while len(s1) != len(s2):
-        if len(s1) < len(s2):
-            s1.append(" ")
-        if len(s2) < len(s1):
-            s2.append(" ")
+    while len(string1) != len(string2):
+        if len(string1) < len(string2):
+            string1.append(" ")
+        if len(string2) < len(string1):
+            string2.append(" ")
     s1 = bytearray(string1, encoding="utf-8")
     s2 = bytearray(string2, encoding="utf-8")
     # while len(s1) != len(s2):
